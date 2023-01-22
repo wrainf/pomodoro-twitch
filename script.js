1fb9d88fb54bba6567b41e6cac16f829b1735946
wrainf <amirfaiz1806@gmail.com>
2023-01-22 15:53:20 +0000

    fixed clock

--- script.js
+++ script.js
@@ -5,9 +5,15 @@ const day = document.getElementById('day');
 const time = document.getElementById('time');
 const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday', 'Friday', 'Saturday']
 
-d = new Date();
-day.textContent = days[d.getDay()];
-time.textContent = `${d.getHours()}:${d.getMinutes()}`
+function currentTime() {
+  const d = new Date();
+  day.textContent = days[d.getDay()];
+  time.textContent = `${d.getHours()}:${d.getMinutes()}`
+  let t = setTimeout(function(){ currentTime() }, 60000); 
+}
+
+currentTime();
+
 function incrementCurr() {
   curr.textContent = parseInt(curr.textContent) + 1;
 }
